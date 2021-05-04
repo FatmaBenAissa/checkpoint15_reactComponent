3290a3fee3dfec5b42c295c9ad13c9f4070c4cd6
fatma <fatmabenaissa1@gmail.com>
2021-05-04 12:43:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import  Photo from './Component/Profile/ProfilePhoto';
+import Name from'./Component/Profile/FullName'
+import Adress from'./Component/Profile/Adress'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div1">
+      <h1 className="title">Welcome to my Profile</h1>
+     
+      <div className="div2">
+        <div><Photo/></div>
+        <div><Name/></div>
+      </div>
+      <div className="div3"><Adress/></div>
     </div>
   );
 }
